e499601fb453c8e74b15e21469fca6d3a8c0158f
mtmsk125 <moawiaalkafaween@gmail.com>
2026-06-16 18:36:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,6 @@ def split():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+   
